529b3a2c32bc8b6a0ec639b7b93c6c4116703e34
Swamsthenerd <Swamsthenerd@users.noreply.github.com>
2020-04-11 20:19:18 +0530

    part 1

---
+++ Water/Assets/MapGenEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGen))]
+public class MapGenEditor : Editor
+{
+    public override void OnInspectorGUI(){
+        MapGen mapGen = (MapGen)target;
+
+        if(DrawDefaultInspector()){
+            if(mapGen.autoupdate){
+                mapGen.GenerateMap();
+            }
+        };
+
+        if (GUILayout.Button("Generate")){
+            mapGen.GenerateMap();
+        }
+    }
+}
